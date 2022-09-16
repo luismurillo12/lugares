a4314a35cce5f2abcf0a3733070c18569555dad4
luismurillo12 <98240656+luismurillo12@users.noreply.github.com>
2022-09-16 11:38:55 -0600

    Cambios en el proyecto, más login

---
+++ app/src/main/java/com/example/lugares_v/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.lugares_v
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lugares_v.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    //Definicion del objeto para la autenticacion
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var  binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth= Firebase.auth
+        //binding.etEmail
+    }
+}
