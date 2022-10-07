431d7bfe323ce1d79d7113e2db563f7d5e033ff5
luismurillo12 <98240656+luismurillo12@users.noreply.github.com>
2022-10-06 21:40:48 -0600

    semana4
    semana4

---
+++ app/src/main/java/com/example/pasientes/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pasientes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lugares_v.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
